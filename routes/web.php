420da390917bacf11c424ff94ee0cf50004ed11a
MARIA JOSE SIMON <mariajose.simon@ikasle.egibide.org>
2021-04-04 16:42:24 +0200

    Rutas.

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [EntradaController::class, 'index']);
+Route::resource('entradas', EntradaController::class);
+Route::resource('comentarios', ComentarioController::class);
